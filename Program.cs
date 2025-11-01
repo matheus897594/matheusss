4ed1eb9733f6448b839f91b66486fe7126de2a9b
Matheus <ma8915004@gmail.com>
2025-10-31 22:35:06 -0300

    atividade finalizada

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int numeroPim = 1;
+
+while(numeroPim <= 40)
+{
+    bool multiploDoQuatro = numeroPim % 4 == 0;//como definir um multiplo
+    if (multiploDoQuatro)
+    {
+        Console.WriteLine("pim");
+        Console.Beep(1650, 66);
+    }
+    else
+    {
+        Console.Write($"{numeroPim} ");
+    }
+    
+    numeroPim = numeroPim + 1;
+}
